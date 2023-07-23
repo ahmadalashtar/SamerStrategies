02c2ef74a247d75d4d9064bc66390cade4f9fda4
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-07-23 10:11:48 +0300

    Go to france

--- candle.js
+++ candle.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const {makeApiKey} = require('./makeApiKey')
 require('dotenv').config()
 
 const convertPirsToString = (pairs)=>{
@@ -12,21 +13,54 @@ const convertPirsToString = (pairs)=>{
     return pairsString
 }
 
-const getCandle = async (pairs) => {
+const getCandles = async (pairs) =>{
+    let candles = []
+    for (const pair of pairs) {
+        const fromPair = pair[0] + pair[1] + pair[2]
+        const toPair = pair[3]+pair[4]+pair[5]
+        const apiKey = makeApiKey(16)
+        const pairsString = convertPirsToString(pairs)
+        const url =
+            `https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=${fromPair}&to_symbol=${toPair}&apikey=${apiKey}`
+        // const url = `https://fcsapi.com/api-v3/forex/candle?symbol=${pairsString}&period=1w&access_key=${process.env.access_key}&candle=active`
 
+        const config = {
+            headers: {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Connection": "keep-alive",
+                "DNT": "1",
+                "Host": "www.alphavantage.co",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "TE": "trailers",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
+            }
+        }
+        try {
+            const response = await axios.get(url, config);
+            const result = response.data['Time Series FX (Weekly)'][Object.keys(response.data['Time Series FX (Weekly)'])[0]]
+            const candle = {
+                'o': result['1. open'],
+                'h': result['2. high'],
+                'l': result['3. low'],
+                'c': result['4. close'],
+                's': pair
+            }
+            candles.push(candle)
 
-    const pairsString = convertPirsToString(pairs)
-    const url = `https://fcsapi.com/api-v3/forex/candle?symbol=${pairsString}&period=1w&access_key=${process.env.access_key}&candle=active`
-
-    try {
-        const response = await axios.get(url);
-        return response.data
-
-      } catch (error) {
-        return { 'Error': error.messag };
-    }
+        } catch (error) {
+            console.log({ 'Error': error.messag });
+            console.log("Challenge Accepted")
+            pairs.push(pair)
+        }
+    }   
+    return candles;
     
     
 }
-
-module.exports = { getCandle }
+module.exports = { getCandles }
--- candleRouter.js
+++ candleRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCandle } = require('./candle')
+const { getCandles } = require('./candle')
 
 // const pairs = ["CHFJPY"]
 const symbols = ['JPY','CHF', 'USD','EUR', 'GBP','CAD','AUD','NZD']
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     let candles;
 
     try {
-        candles = await getCandle(pairs);
+        candles = await getCandles(pairs);
         // const result = {
         //     'pair': candle['pair'],
         //     'state': candle['state'] == 'thin' ? 'ارتدادية' : 'دسمة',
@@ -27,13 +27,53 @@ router.get('/', async (req, res) => {
         // pairs.push(pair)
     }
 
-    // let displayedCandles = [];
+    let displayedCandles = [];
+    for (const candle of candles) {
+        let o = Number(candle['o']);
+        let c = Number(candle['c']);
+        const l = Number(candle['l']);
+        const h = Number(candle['h']);
+        const pair = candle['s'];
+
+        const u = (o > c ? o : c);
+        const d = (c < o ? c : o);
+        const upperShadow = h - u;
+        const lowerShadow = d - l;
+        const body = u - d;
+        const upperShadowUI = upperShadow * 100 / (upperShadow + lowerShadow + body)
+        const lowerShadowUI = lowerShadow * 100 / (upperShadow + lowerShadow + body)
+        const bodyUI = 100 - lowerShadowUI - upperShadowUI;
+
+        let state;
+        if (body < upperShadow || body < lowerShadow) {
+            state = 'ارتدايدة'
+        } else {
+            state = 'دسمة'
+        }
+
+
+        const displayCandle = {
+            'pair': pair,
+            'high': h,
+            'low': l,
+            'open': u,
+            'close': d,
+            'color': o > c ? 'red' : 'green',
+            'state': state,
+            'upperShadow': upperShadowUI,
+            'lowerShadow': lowerShadowUI,
+            'body':bodyUI
+
+        }
+            console.log(displayCandle)
+        
+        displayedCandles.push(displayCandle);
+    }
     
 
 
 
-    // res.render('candleView.ejs',{candles: displayedCandles})
-    res.send(candles)
+    res.render('candleView.ejs',{candles: displayedCandles})
 
     //     const arabicState = state == 'thin' ? 'ارتدادية' : 'دسمة'
     //     pair = pair[0] + pair[1] + pair[2] + " " + pair[3] + pair[4] + pair[5]
