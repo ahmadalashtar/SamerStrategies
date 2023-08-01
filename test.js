749a32cc9f65605542b87fa2e4e520b5d5dcac98
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-08-01 21:18:32 +0300

    Logging

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-const JSDOM = require("jsdom").JSDOM;
 const fetch = require('node-fetch');
 
 const test = async () => {
@@ -6,15 +5,6 @@ const test = async () => {
     const baseURL = `https://www.myfxbook.com/forex-market/currencies/USDJPY-historical-data`
     const response = await fetch(baseURL);
     const data = await response.text();
-    const dom = new JSDOM(data);
-    const window = dom.window;
-    const document = window.document;
-    const items = document.getElementsByName('openUSDJPY')
-    const item = items[1]
-    const inner = item.innerHTML
-    console.log(items)
-    console.log(item)
-    console.log(inner)
     
 
     return 'hello';
