3deb4049dd805c7306fb59751dbace16d1cb68ac
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-08-01 21:39:14 +0300

    Logging

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
-
+const  {parse}  =require('node-html-parser');
 const test = async () => {
 
     const baseURL = `https://www.myfxbook.com/forex-market/currencies/USDJPY-historical-data`
     const response = await fetch(baseURL);
     const data = await response.text();
-    
-
-    return 'hello';
+    const root = parse(data);
+    const items = root.querySelectorAll('span[name="timeUSDJPY"]')
+    return items[0].innerHTML;
 }
 
 module.exports = { test }
